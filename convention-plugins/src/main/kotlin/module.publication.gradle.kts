10e79ffdd4381ef0947b9806791684c1107b92a3
huarangmeng <844707746@qq.com>
2024-04-30 11:20:48 +0800

    修改maven publish 内容

--- convention-plugins/src/main/kotlin/module.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/module.publication.gradle.kts
@@ -18,9 +18,9 @@ publishing {
 
         // Provide artifacts information required by Maven Central
         pom {
-            name.set("Kotlin Multiplatform library template")
-            description.set("Dummy library to test deployment to Maven Central")
-            url.set("https://github.com/Kotlin/multiplatform-library-template")
+            name.set("uuid")
+            description.set("Kotlin Multiplaform UUID")
+            url.set("https://github.com/huarangmeng/uuid")
 
             licenses {
                 license {
@@ -30,14 +30,14 @@ publishing {
             }
             developers {
                 developer {
-                    id.set("JetBrains")
-                    name.set("JetBrains Team")
-                    organization.set("JetBrains")
-                    organizationUrl.set("https://www.jetbrains.com")
+                    id.set("huaranmeng")
+                    name.set("huaranmeng")
+                    organization.set("huaranmeng")
+                    organizationUrl.set("https://github.com/huarangmeng")
                 }
             }
             scm {
-                url.set("https://github.com/Kotlin/multiplatform-library-template")
+                url.set("https://github.com/huarangmeng/uuid")
             }
         }
     }
