ed82d247ebe3bedca87808ff0c12b65f5005e52c
huarangmeng <844707746@qq.com>
2024-05-07 11:57:27 +0800

    打印内容检查

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -14,6 +14,8 @@ nexusPublishing {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username = "RTw0mxl0"
+            password = "wmdsxuxCiZT0GAKccexqNkCRall99OgsmS4HkYLzpznQ"
         }
     }
 }
